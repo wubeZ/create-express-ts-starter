b2b7d66f2eb2f5b623d790e37766b7383b7268c3
mac <wubezeleke@gmail.com>
2024-03-27 14:06:23 +0300

    Fix: Add the mongoDB connection to entry point

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app'
 import * as os from 'os'
 import logger from './common/logger'
+import mongodb from './services/db'
 
 // to use env variables
 import './common/env'
@@ -9,4 +10,5 @@ const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   logger.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname()} on port ${PORT}`)
+  mongodb.Connection
 })
